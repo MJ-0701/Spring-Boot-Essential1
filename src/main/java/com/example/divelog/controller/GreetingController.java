842142cedc5f2d5650b9cbf9a32bfebc84d8788d
MJ-0701 <jack2718@naver.com>
2023-03-24 19:32:13 +0900

    feat(setting) : update

--- src/main/java/com/example/divelog/controller/GreetingController.java
+++ src/main/java/com/example/divelog/controller/GreetingController.java
@@ -1,7 +1,6 @@
-package com.example.greeting.controller;
+package com.example.divelog.controller;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
